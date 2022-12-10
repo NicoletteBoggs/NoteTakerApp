bccc9a848d965d3c023f34d90f35c3cd0526be22
NicoletteBoggs <nrb1628@gmail.com>
2022-12-09 18:02:17 -0700

    added delete feature

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NoteTakerApp
+
 An application called Note Taker that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+
+The user is also able to delete their notes.
--- controller.js
+++ controller.js
@@ -16,4 +16,16 @@ router.post("/notes", (req, res) => {
   fs.writeFileSync("./db/db.json", JSON.stringify(dbjson));
   res.json(dbjson);
 });
+router.delete("/notes/:id", (req, res) => {
+  let newdb = [];
+  // iterate over dbjson array and push current index note into new db array if its id does not match the req.params id of the note we are trying to throw away
+  for (var i = 0; i < dbjson.length; i++) {
+    if (dbjson[i].id != req.params.id) {
+      newdb.push(dbjson[i]);
+    }
+  }
+  dbjson = newdb;
+  fs.writeFileSync("./db/db.json", JSON.stringify(dbjson));
+  res.json(dbjson);
+});
 module.exports = router;
